2b0dab44b236fcf83109532276a02386c390fe5b
Antônio Marcos <anttonimarco@gmail.com>
2024-11-20 01:32:59 -0300

    Alura

---
+++ Alura/Java criando a sua primeira aplicação/Leitura.java
@@ -0,0 +1,20 @@
+package src;
+import java.util.Scanner;
+
+public class Leitura {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digite seu filme favorito");
+        String filme = leitura.nextLine();
+        System.out.println("Qual o ano de lançamento? ");
+        int anoDeLancamento = leitura.nextInt();
+        System.out.println("Diga sua avaliação para o filme ");
+        double avaliação = leitura.nextDouble();
+
+        System.out.println(filme);
+        System.out.println(anoDeLancamento);
+        System.out.println(avaliação);
+    }
+}
+
---
+++ Alura/Java criando a sua primeira aplicação/Teste.java
@@ -0,0 +1,19 @@
+public class Teste {
+    
+
+    public static void main(String[] args) {
+        int contador = 1;
+    
+        while(contador <= 10) {
+            System.out.println(contador++);
+        }
+
+
+
+        for(int y = 1; y <= 10; y++) {
+            System.out.println(y);
+        }
+    }
+}
+    
+
